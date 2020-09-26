721048535bb527f06ddff2588c98547f56f1cdd8
KremerStas <red-bull1951@yandex.ru>
2020-09-27 01:18:19 +0300

    change path

--- main.py
+++ main.py
@@ -2,31 +2,33 @@ from models import Song
 import random
 import xml.etree.ElementTree as ET
 from defusedxml import minidom
+import os
 
-avtor = input().split(', ')  # Формат записи (Название группы-3, название группы-2)
-pesni = []
+author = input().split(', ')  # Формат записи (Название группы-3, название группы-2)
+songs = []
 
 root = ET.Element('CATALOG')
 
-for zapros in avtor:
+for requests in author:
     elem = ET.SubElement(root, 'CD')
-    command = zapros.split('-')
+    command = requests.split('-')
     for art in Song.select().where(Song.artist == command[0]):
-        pesni.append(art.song)
-    if pesni:
+        songs.append(art.song)
+    if songs:
         elem_artist = ET.SubElement(elem, 'ARTIST')
         elem_artist.text = str(command[0])
         for i in range(int(command[1])):
             elem_title = ET.SubElement(elem, 'TITLE')
-            title = str(random.choice(pesni))
+            title = str(random.choice(songs))
             elem_title.text = title
     else:
         print('Исполнитель не найден')
-    pesni.clear()
+    songs.clear()
 
 tree = ET.ElementTree(root)
-put = r'C:\Users\Пользователь\Desktop\example.xml'
-tree.write(put)
+path = os.getcwd()
+path_xml = os.path.join(path, 'example.xml')
+tree.write(path_xml)
 xml = minidom.parseString(ET.tostring(tree.getroot())).toprettyxml()
-f = open(put, 'w', encoding='utf-8')
+f = open(path_xml, 'w', encoding='utf-8')
 f.write(xml)
